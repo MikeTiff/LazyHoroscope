f9440829a955ecdb78533fa42948c18df11c03a6
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-12 17:29:45 -0800

    Inputs with nickname and number output

--- main.cpp
+++ main.cpp
@@ -8,14 +8,16 @@ int main()
 
 {
   string First, Last;
+  int Lucky = 0;
   //declare variables
   cout<< "Please enter your fist name.\n";
   cin>> First;
   cout<< "Please enter your last name.\n";
   cin>> Last;
   //get user input
-
+  cout<< "Welcome, "<<First[0]<<Last[0]<<", here is your fortune\n";
   //tell fortune
-
+  Lucky = ((First.length())+Last.length());
+  cout<< "Your lucky number is "<< Lucky<< endl;
   return 0;
 }
