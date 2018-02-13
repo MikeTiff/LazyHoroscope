4fe4df0ba59c3f7a2489052ff900f898f4838527
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-12 17:19:17 -0800

    First and Last name input

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-//Authors:
+//Authors: Michael Tiffany
 #include<iostream>
 #include<string>
 
 using namespace std;
 
 int main()
-{
-  //declare variables
 
+{
+  string First, Last;
+  //declare variables
+  cout<< "Please enter your fist name.\n";
+  cin>> First;
+  cout<< "Please enter your last name.\n";
+  cin>> Last;
   //get user input
 
   //tell fortune
